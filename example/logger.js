2c978fb51573d477a2ea78456e1f9197c03090e6
Yuning Guo <gyn2014@reachjunction.com>
2015-06-06 14:34:31 +0800

    更新模块输出格式

---
+++ example/logger.js
@@ -0,0 +1,36 @@
+var log4js = require("log4js");
+var log4jx = require("../log4jx");
+
+log4jx(log4js, {
+  path         : __dirname,
+  format: "[@date] [@level][@pid][@worker] @category - @data (@file:[@method]:@line:@column)"
+});
+
+log4js.configure({
+  appenders     : [
+    {
+      type: 'console'
+    }
+  ],
+  replaceConsole: true
+});
+
+function getLogger(name, level) {
+  var dateFileLog = log4js.getLogger(name);
+  level = level || log4js.levels.INFO;
+  dateFileLog.setLevel(level);
+  return dateFileLog;
+}
+
+var logger = getLogger("category", 'DEBUG');
+
+function getApple() {
+  console.log("测试内容console");
+  logger.info("测试内容0");
+  logger.debug("测试内容1");
+  logger.debug("测试内容1", {name:"tom"},{age1:100});
+  logger.warn("测试内容2");
+  logger.error("测试内容3");
+}
+
+getApple();
--- log4jx.js
+++ log4jx.js
@@ -1,52 +1,48 @@
 var path = require("path");
+var util = require('util');
+var cluster = require("cluster");
+var dateFormat = require('./lib/date_format');
+
 var colours = {
-  ALL: "grey",
+  ALL  : "grey",
   TRACE: "blue",
   DEBUG: "cyan",
-  INFO: "green",
-  WARN: "yellow",
+  INFO : "green",
+  WARN : "yellow",
   ERROR: "red",
   FATAL: "magenta",
-  OFF: "grey"
-};
-var styles = {
-  //styles
-  'bold'      : [1,  22],
-  'italic'    : [3,  23],
-  'underline' : [4,  24],
-  'inverse'   : [7,  27],
-  //grayscale
-  'white'     : [37, 39],
-  'grey'      : [90, 39],
-  'black'     : [90, 39],
-  //colors
-  'blue'      : [34, 39],
-  'cyan'      : [36, 39],
-  'green'     : [32, 39],
-  'magenta'   : [35, 39],
-  'red'       : [31, 39],
-  'yellow'    : [33, 39]
+  OFF  : "grey"
 };
 
-function colorizeStart(style) {
-  return style ? '\x1B[' + styles[style][0] + 'm' : '';
-}
-function colorizeEnd(style) {
-  return style ? '\x1B[' + styles[style][1] + 'm' : '';
-}
-function colorize (str, style) {
-  return colorizeStart(style) + str + colorizeEnd(style);
-}
+var styles = {
+  //styles
+  'bold'     : [1, 22],
+  'italic'   : [3, 23],
+  'underline': [4, 24],
+  'inverse'  : [7, 27],
+  //grayscale
+  'white'    : [37, 39],
+  'grey'     : [90, 39],
+  'black'    : [90, 39],
+  //colors
+  'blue'     : [34, 39],
+  'cyan'     : [36, 39],
+  'green'    : [32, 39],
+  'magenta'  : [35, 39],
+  'red'      : [31, 39],
+  'yellow'   : [33, 39]
+};
 
 // override instance methods
 function extend(log4js) {
   var logger = log4js.getLogger();
+  log4js.layouts.colouredLayout = colouredLayout;
   ["trace", "debug", "info", "warn", "error", "fatal"].forEach(function (method) {
     var original = logger.constructor.prototype[method];
     logger.constructor.prototype[method] = function log() {
-      var args = [].slice.call(arguments),
-        trace = getTrace(log);
-      args.push(colorize(formatter(trace),colours[method.toUpperCase()]));
+      var args = [].slice.call(arguments);
+      var trace = getTrace(log);
+      args.push(colorize(format(trace, exports.format.split('@data')[1]), colours[method.toUpperCase()]));
       return original.apply(this, args);
     };
   });
@@ -55,29 +51,26 @@ function extend(log4js) {
 function prepareStackTrace(error, structuredStackTrace) {
   var trace = structuredStackTrace[0];
   return {
-    // method name
-    name: trace.getMethodName() || trace.getFunctionName() || "<anonymous>",
     // file name
-    file: trace.getFileName(),
+    file  : trace.getFileName(),
     // line number
-    line: trace.getLineNumber(),
+    line  : trace.getLineNumber(),
     // column number
-    column: trace.getColumnNumber()
+    column: trace.getColumnNumber(),
+    // method name
+    method: trace.getMethodName() || trace.getFunctionName() || "<anonymous>"
   };
 }
 
 function getTrace(caller) {
-  var original = Error.prepareStackTrace,
-    error = {};
+  var error = {};
   Error.captureStackTrace(error, caller || getTrace);
   Error.prepareStackTrace = prepareStackTrace;
-  var stack = error.stack;
-  Error.prepareStackTrace = original;
-  return stack;
+  return error.stack;
 }
 
 // format trace
-function formatter(trace) {
+function format(trace, format) {
   if (trace.file) {
     // absolute path -> relative path
     exports.path && (trace.file = path.relative(exports.path, trace.file));
@@ -85,21 +78,94 @@ function formatter(trace) {
     trace.file = "";
   }
 
-  return exports.format
-    .split("@name").join(trace.name)
+  return format
     .split("@file").join(trace.file)
     .split("@line").join(trace.line)
-    .split("@column").join(trace.column);
+    .split("@method").join(trace.method)
+    .split("@column").join(trace.column)
+
+    //.split("@data").join(formatLogData(trace.data))
+
+    .split("@date").join(trace.startTime ? dateFormat.asString(trace.startTime) : '@date')
+    .split("@level").join(trace.level)
+    .split("@category").join(trace.categoryName)
+    .split("@pid").join(process.pid || '@pid')
+    .split("@port").join(process.port || '@port')
+    .split("@worker").join(((cluster.worker && cluster.worker.id) ? "worker-" + cluster.worker.id : "master"));
+}
+
+
+function colorizeStart(style) {
+  return style ? '\x1B[' + styles[style][0] + 'm' : '';
+}
+
+function colorizeEnd(style) {
+  return style ? '\x1B[' + styles[style][1] + 'm' : '';
+}
+
+function colorize(str, style) {
+  return colorizeStart(style) + str + colorizeEnd(style);
+}
+
+
+/**
+ * colouredLayout - taken from masylum's fork.
+ * same as basicLayout, but with colours.
+ */
+function colouredLayout(loggingEvent) {
+  return timestampLevelAndCategory(
+      loggingEvent,
+      colours[loggingEvent.level.toString()]
+    ) + formatLogData(loggingEvent.data);
+}
+
+function timestampLevelAndCategory(loggingEvent, colour) {
+  return colorize(
+    format(loggingEvent, exports.format.split('@data')[0])
+    , colour
+  );
+}
+
+function formatLogData(logData) {
+  var data = Array.isArray(logData) ? logData : Array.prototype.slice.call(arguments);
+  return util.format.apply(util, wrapErrorsWithInspect(data));
+}
+
+function wrapErrorsWithInspect(items) {
+  return items.map(function (item) {
+    if ((item instanceof Error) && item.stack) {
+      return {
+        inspect: function () {
+          return util.format(item) + '\n' + item.stack;
+        }
+      };
+    } else {
+      return item;
+    }
+  });
 }
 
 var extended = false;
+
 exports = module.exports = function (log4js, options) {
   extended || extend(log4js);
   extended = true;
 
   // init
   exports.path = null;
-  exports.format = "at @name (@file:@line:@column)";
+  exports.format = "[@date] [@level] @category - @data [@method] (@file:@line:@column)";
+
+  if (options && options.format) {
+    var dataIndex = options.format.indexOf('@data');
+    if (dataIndex === -1) {
+      throw new Error('@data require in format param');
+    }
+    if (options.format.indexOf('@date') > dataIndex
+      || options.format.indexOf('@level') > dataIndex
+      || options.format.indexOf('@category') > dataIndex) {
+      throw new Error('@data must after @date,@level,@category');
+    }
+  }
 
   options || (options = {});
   options.path && (exports.path = options.path);
